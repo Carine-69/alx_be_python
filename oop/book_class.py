dd621c54fb1eea42b9f89789fb40b452ccb15456
carine-69 <umugabecarine@gmail.com>
2024-10-09 14:46:13 +0200

    book class

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,20 +1,32 @@
 class Book:
     def __init__(self, title, author, year):
+        """Constructor to initialize the Book instance."""
         self.title = title
         self.author = author
         self.year = year
-
-    def __str__(self):
-        return f'{self.title} by {self.author}, was published in {self.year}.'""
+        print(f"Creating Book: {self.title} by {self.author}, published in {self.year}.")
 
     def __del__(self):
-        print(f'Deleting {self.author}.')
+        """Destructor to print a message when the Book object is deleted."""
+        print(f"Deleting '{self.title}'")
+
+    def __str__(self):
+        """String representation of the Book instance."""
+        return f"'{self.title}' by {self.author}, published in {self.year}"
 
     def __repr__(self):
+        """Official representation of the Book instance."""
         return f"Book('{self.title}', '{self.author}', {self.year})"
 
 
-Book1 = Book("self improvement", "Max Milliano", 1998)
+# Creating an instance of the Book class
+book1 = Book("1984", "George Orwell", 1949)
 
-print(Book1)
+# Printing the string representation of the book
+print(book1)  # Output: '1984' by George Orwell, published in 1949
 
+# Printing the official representation of the book
+print(repr(book1))  # Output: Book('1984', 'George Orwell', 1949)
+
+# Deleting the instance
+del book1
