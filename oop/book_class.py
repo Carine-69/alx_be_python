c6bd573481121885003433509273de2fa93b618c
carine-69 <umugabecarine@gmail.com>
2024-10-09 14:55:40 +0200

    book class

--- oop/book_class.py
+++ oop/book_class.py
@@ -4,15 +4,14 @@ class Book:
         self.title = title
         self.author = author
         self.year = year
-        print(f"Creating Book: {self.title} by {self.author}, published in {self.year}.")
 
     def __del__(self):
         """Destructor to print a message when the Book object is deleted."""
-        print(f"Deleting '{self.title}'")
+        print(f"Deleting {self.title}")
 
     def __str__(self):
         """String representation of the Book instance."""
-        return f"'{self.title}' by {self.author}, published in {self.year}"
+        return f"{self.title} by {self.author}, published in {self.year}"
 
     def __repr__(self):
         """Official representation of the Book instance."""
